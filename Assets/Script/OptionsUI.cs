422c91504232e4605e649af1051665e6032cbacd
Jan Adam K. Berdan <jakberdan312@gmail.com>
2023-05-31 16:28:35 +0800

    - Re-edit Main Menu, Cube and Camera
    - Changed the Options/Settings via Removing Fullscreen and Resolution, then turn it into FPS Limits
    - Fixed the Cube rotation properly
    - Fixed the Camera Angle Hopefully
    - Added a prefab of Target for the Cube's Rotation

--- Assets/Script/OptionsUI.cs
+++ Assets/Script/OptionsUI.cs
@@ -10,47 +10,20 @@ public class OptionsUI : MonoBehaviour
     [SerializeField] private GameObject MainMenuUI;
     [SerializeField] private GameObject OptionsUIHolder;       
     public AudioMixer audioMixer;  
-    public TMPro.TMP_Dropdown resolutionDropdown; 
+    public TMPro.TMP_Dropdown FPSDropdown; 
     public TMPro.TMP_Dropdown graphicsQuality;
-    Resolution[] resolutions;
     public float volumeValue;
-    public int qualityIndexValue;
-    public static bool bFullscreen;
     public Slider volumeSlider;
 
     void Start()
     {
-        Screen.fullScreen = bFullscreen;
-
-        Screen.SetResolution(PlayerPrefs.GetInt("resolutionWidth"), PlayerPrefs.GetInt("resolutionHeight"), Screen.fullScreen);
+        Application.targetFrameRate = PlayerPrefs.GetInt("FPSLimit");
+        FPSDropdown.value = PlayerPrefs.GetInt("FPSLimitValue");
 
         QualitySettings.SetQualityLevel(PlayerPrefs.GetInt("qualityValue"));
         graphicsQuality.value = PlayerPrefs.GetInt("qualityValue");
 
         volumeSlider.value = PlayerPrefs.GetFloat("MasterVolume");
-
-
-        resolutions = Screen.resolutions;
-
-        resolutionDropdown.ClearOptions();
-
-        List<string> options = new List<string>();
-
-        int currentResolutionIndex = 0;
-        for (int i = 0; i < resolutions.Length; i++)
-        {
-            string option = resolutions[i].width + " x " + resolutions[i].height;
-            options.Add(option);
-
-            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
-            {
-                currentResolutionIndex = i;
-            }
-        }
-
-        resolutionDropdown.AddOptions(options);
-        resolutionDropdown.value = currentResolutionIndex;
-        resolutionDropdown.RefreshShownValue();
     }
     public void OptionReturn()
     {
@@ -71,17 +44,21 @@ public class OptionsUI : MonoBehaviour
         QualitySettings.SetQualityLevel(PlayerPrefs.GetInt("qualityValue"));
     }
 
-    public void SetFullscreen(bool isFullscreen)
+    public void SetFPSLimit (int FPSIndex)
     {
-        bFullscreen = isFullscreen;
-        Screen.fullScreen = bFullscreen;
-    }
-
-    public void SetResolution (int resolutionIndex)
-    {
-        Resolution resolution = resolutions[resolutionIndex];
-        Screen.SetResolution(PlayerPrefs.GetInt("resolutionWidth"), PlayerPrefs.GetInt("resolutionHeight"), Screen.fullScreen);
-        PlayerPrefs.SetInt("resolutionWidth", resolution.width);
-        PlayerPrefs.SetInt("resolutionHeight", resolution.height);
+        PlayerPrefs.SetInt("FPSLimitValue", FPSIndex);
+        switch (FPSIndex)
+        {
+            case 0:
+                PlayerPrefs.SetInt("FPSLimit", 30); 
+                Application.targetFrameRate = PlayerPrefs.GetInt("FPSLimit");
+                break;
+            case 1:
+                PlayerPrefs.SetInt("FPSLimit", 60); 
+                Application.targetFrameRate = PlayerPrefs.GetInt("FPSLimit");
+                break;
+            default:
+                break;
+        }
     }
 }
