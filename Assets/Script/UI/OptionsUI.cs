e158b6abf9926b563a69cb49958dcb9051fc6512
kazunya14 <79516308+kazunya14@users.noreply.github.com>
2023-06-14 15:35:34 +0900

    Added Missing Files

--- Assets/Script/UI/OptionsUI.cs
+++ Assets/Script/UI/OptionsUI.cs
@@ -1,63 +1,56 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.Audio;
 using UnityEngine.UI;
+using Lowscope.Saving;
 
-public class OptionsUI : MonoBehaviour
-{
+public class OptionsUI : MonoBehaviour {
     [SerializeField] private GameObject MainMenuUI;
-    [SerializeField] private GameObject OptionsUIHolder;       
-    public AudioMixer audioMixer;  
-    public TMPro.TMP_Dropdown FPSDropdown; 
-    public TMPro.TMP_Dropdown graphicsQuality;
+    [SerializeField] private GameObject OptionsUIHolder;
+    public AudioMixer audioMixer;
+    public TMP_Dropdown FPSDropdown;
+    public TMP_Dropdown graphicsQuality;
     public float volumeValue;
     public Slider volumeSlider;
 
-    void Start()
-    {
+    private void Start() {
         Application.targetFrameRate = PlayerPrefs.GetInt("FPSLimit");
         //FPSDropdown.value = PlayerPrefs.GetInt("FPSLimitValue");
 
-        QualitySettings.SetQualityLevel(PlayerPrefs.GetInt("qualityValue"));
-        graphicsQuality.value = PlayerPrefs.GetInt("qualityValue");
+        QualitySettings.SetQualityLevel(SaveMaster.GetInt("qualityValue"));
+        graphicsQuality.value = SaveMaster.GetInt("qualityValue");
 
         volumeSlider.value = PlayerPrefs.GetFloat("MasterVolume");
     }
-    public void OptionReturn()
-    {
+
+    public void OptionReturn() {
         MainMenuUI.SetActive(true);
         OptionsUIHolder.SetActive(false);
     }
 
-    public void SetVolume (float volume)
-    {
+    public void SetVolume(float volume) {
         volumeValue = volume;
         audioMixer.SetFloat("MasterVolume", volumeValue);
-        PlayerPrefs.SetFloat("MasterVolume", volumeValue); 
+        SaveMaster.SetFloat("MasterVolume", volumeValue);
+        PlayerPrefs.SetFloat("MasterVolume", volumeValue);
     }
 
-    public void SetQuality (int qualityIndex)
-    {
-        PlayerPrefs.SetInt("qualityValue", qualityIndex); 
+    public void SetQuality(int qualityIndex) {
+        PlayerPrefs.SetInt("qualityValue", qualityIndex);
         QualitySettings.SetQualityLevel(PlayerPrefs.GetInt("qualityValue"));
     }
 
-    public void SetFPSLimit (int FPSIndex)
-    {
+    public void SetFPSLimit(int FPSIndex) {
         PlayerPrefs.SetInt("FPSLimitValue", FPSIndex);
-        switch (FPSIndex)
-        {
+        switch(FPSIndex) {
             case 0:
-                PlayerPrefs.SetInt("FPSLimit", 30); 
+                PlayerPrefs.SetInt("FPSLimit", 30);
                 Application.targetFrameRate = PlayerPrefs.GetInt("FPSLimit");
                 break;
+
             case 1:
-                PlayerPrefs.SetInt("FPSLimit", 60); 
+                PlayerPrefs.SetInt("FPSLimit", 60);
                 Application.targetFrameRate = PlayerPrefs.GetInt("FPSLimit");
-                break;
-            default:
                 break;
         }
     }
