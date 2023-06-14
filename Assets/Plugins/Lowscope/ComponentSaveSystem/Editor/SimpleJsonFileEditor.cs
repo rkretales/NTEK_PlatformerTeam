c1d07a1581faf504699a07bdfe88b6c176e531a5
kazunya14 <79516308+kazunya14@users.noreply.github.com>
2023-06-14 15:10:34 +0900

    Added ComponentSaveSystem

---
+++ Assets/Plugins/Lowscope/ComponentSaveSystem/Editor/SimpleJsonFileEditor.cs
@@ -0,0 +1,106 @@
+﻿using System.IO;
+using Lowscope.Saving.Data;
+using UnityEditor;
+using UnityEngine;
+using FileMode = System.IO.FileMode;
+
+public class SimpleJsonFileEditor : EditorWindow
+{
+	string filePath = "";
+	private string fileContents = "";
+	private string searchForExtension = "";
+	private Vector2 scroll;
+	private bool legacySave;
+
+	[MenuItem ("Window/Saving/Tools/Simple Json File Editor")]
+	public static void  ShowWindow () {
+		GetWindow(typeof(SimpleJsonFileEditor), false, "Simple Json File Editor");
+	}
+
+	private void OnBecameVisible()
+	{
+		if (string.IsNullOrEmpty(searchForExtension))
+		{
+			searchForExtension = SaveSettings.Get().fileExtensionName.Substring(1);
+		}
+
+		string p = EditorPrefs.GetString("SimpleSaveFileEditor_LastFile", "");
+		if (string.IsNullOrEmpty(p) || !File.Exists(p)) 
+			return;
+		
+		fileContents = File.ReadAllText(p);
+		filePath = p;
+	}
+
+	void OnGUI ()
+	{
+		GUILayout.BeginHorizontal();
+		
+		if (GUILayout.Button("Select File"))
+		{
+			string dir = Path.Combine(Application.persistentDataPath, SaveSettings.Get().fileFolderName);
+			string path = EditorUtility.OpenFilePanel("Open save file", dir , searchForExtension);
+			if (path.Length != 0)
+			{
+				GUI.FocusControl(null);
+				fileContents = File.ReadAllText(path);
+				filePath = path;
+
+				EditorPrefs.SetString("SimpleSaveFileEditor_LastFile", filePath);
+			}
+		}
+
+		GUI.enabled = !string.IsNullOrEmpty(filePath);
+		if (GUILayout.Button("Reload File", GUILayout.MaxWidth(100)))
+		{
+			GUI.FocusControl(null);
+			fileContents = File.ReadAllText(filePath);
+		}
+		GUI.enabled = true;
+		
+		GUILayout.EndHorizontal();
+		
+		GUI.enabled = false;
+		EditorGUIUtility.labelWidth = GUI.skin.label.CalcSize(new GUIContent("File Path")).x + 20;
+		filePath = EditorGUILayout.TextField ("File Path", filePath);
+		GUI.enabled = true;
+		
+		
+		GUI.enabled = !string.IsNullOrEmpty(filePath);
+		scroll = EditorGUILayout.BeginScrollView(scroll);
+		fileContents = EditorGUILayout.TextArea(fileContents, GUILayout.ExpandHeight(true));
+		EditorGUILayout.EndScrollView();
+		
+		
+		GUILayout.BeginHorizontal();
+		if (GUILayout.Button("Write Changes"))
+		{
+			GUI.FocusControl(null);
+			// Automatically remove anything before json starts.
+			fileContents = fileContents.Substring(fileContents.IndexOf('{'));
+			
+			if (legacySave)
+			{
+				using (var writer = new BinaryWriter(File.Open(filePath, FileMode.Create)))
+				{
+					writer.Write(fileContents);
+				}
+			}
+			else
+			{
+				File.WriteAllText(filePath, fileContents);
+			}
+			
+			fileContents = File.ReadAllText(filePath);
+			
+			Debug.Log("[SimpleJsonFileEditor] Saved File!");
+		}
+
+		EditorGUIUtility.labelWidth = GUI.skin.label.CalcSize(new GUIContent("Legacy Save")).x;
+		legacySave = EditorGUILayout.Toggle("Legacy Save", legacySave, GUILayout.MaxWidth(90));
+
+		GUILayout.EndHorizontal();
+		
+		GUI.enabled = true;
+	}
+}
---
+++ Assets/Plugins/Lowscope/ComponentSaveSystem/Examples/Example Main Menu and Pause Menu/Scripts/ExampleMainMenu.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using Lowscope.Saving;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Lowscope.Saving.Examples
+{
+    public class ExampleMainMenu : MonoBehaviour
+    {
+        [Header("References")]
+        [SerializeField] private Button buttonContinue;
+        [SerializeField] private Button buttonNew;
+        [SerializeField] private Button buttonLoad;
+        [SerializeField] private Button buttonQuit;
+
+        [SerializeField] private ExampleErrorScreen errorMessage;
+        [SerializeField] private ExampleSlotMenu slotMenu;
+
+        [Header("Configuration")]
+        [SerializeField] private string sceneToLoadOnNewGame;
+
+        private int lastUsedValidSlot;
+
+        private void Start()
+        {
+            lastUsedValidSlot = SaveMaster.GetLastUsedValidSlot();
+            buttonContinue.interactable = lastUsedValidSlot != -1;
+
+            buttonContinue.onClick.AddListener(Continue);
+            buttonNew.onClick.AddListener(NewGame);
+            buttonLoad.onClick.AddListener(LoadGame);
+            buttonQuit.onClick.AddListener(QuitGame);
+
+            SaveMaster.OnDeletedSave += OnDeletedSave;
+            SaveMaster.OnLoadingFromDiskCorrupt += OnLoadCorrupt;
+        }
+
+        private void OnDestroy()
+        {
+            SaveMaster.OnDeletedSave -= OnDeletedSave;
+            SaveMaster.OnLoadingFromDiskCorrupt -= OnLoadCorrupt;
+        }
+        
+        private void OnLoadCorrupt(int slot)
+        {
+            slotMenu.gameObject.SetActive(false);
+            
+            errorMessage.Configure("Unable to load save file",
+                "The save file might potentially be corrupt. \n" +
+                "Please contact the developer for the game for support.");
+            
+            // In case you want the game to just load.
+            // The save system uses two files to prevent corruption cases.
+            // So if save A fails, it tries to load save B and vice versa (attempts to load newest file first).
+            // You can toggle "Archive Save File On Full Corruption" in Window/Saving/Open Save Settings
+            // And then call the set slot again here. Using SaveMaster.SetSlot(slot, true);
+        }
+
+        private void OnDeletedSave(int obj)
+        {
+            if (lastUsedValidSlot == obj)
+                buttonContinue.interactable = false;
+        }
+
+        private void Continue()
+        {
+            SaveMaster.SetSlotToLastUsedSlot(true);
+        }
+
+        private void LoadGame()
+        {
+            slotMenu.gameObject.SetActive(true);
+        }
+
+        private void QuitGame()
+        {
+            Application.Quit();
+        }
+
+        private void NewGame()
+        {
+            int slot;
+            if (SaveMaster.SetSlotToNewSlot(false, out slot))
+            {
+                SceneManager.LoadScene(sceneToLoadOnNewGame);
+            }
+            else
+            {
+                errorMessage.Configure("All slots full",
+                    "No more available save slots. \n" +
+                    "Please overwrite or remove a slot");
+            }
+        }
+    }
+}
