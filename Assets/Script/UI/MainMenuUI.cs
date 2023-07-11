3a87d697ef3be192b2360446859c4aceebff7479
Rhay Angelo V. Laurito <105723478+LauritoFEUA@users.noreply.github.com>
2023-07-11 14:59:58 +0800

    Bug Fix: Volume & UI Buttons
    :diamond_shape_with_a_dot_inside: Changed volume things. again... Default volume should be:
    Master = 50%
    Music = 100%
    Sfx = 100%

--- Assets/Script/UI/MainMenuUI.cs
+++ Assets/Script/UI/MainMenuUI.cs
@@ -1,14 +1,24 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using MoreMountains.Feedbacks;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using MoreMountains.Tools;
 
 public class MainMenuUI : MonoBehaviour
 {
     [SerializeField] private GameObject MainMenuUIHolder;
     [SerializeField] private GameObject OptionsUIHolder;
-    
+    [SerializeField] private MMSoundManager _soundManager;
+
+    public void Start()
+    {
+        _soundManager = FindObjectOfType(typeof(MMSoundManager)) as MMSoundManager;
+        
+    }
+
     public void StartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
